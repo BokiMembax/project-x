dd6feee496b620a3e3291694b7c665908bc6d9f3
PETARKING <petar.jovevski12@gmail.com>
2024-09-17 14:10:57 +0200

    Added login, company component and base guard

--- webApp/projectXApp/src/app/app.routes.ts
+++ webApp/projectXApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { BaseGuard } from './auth/base.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'company', 
+        loadComponent: () => import('./company/company.component')
+            .then(m => m.CompanyComponent), canActivate: [BaseGuard] }, // Protected route
+    { path: 'login', component: LoginComponent }, // Unprotected login route
+    { path: '', redirectTo: '/company', pathMatch: 'full' } // Default route
+];
