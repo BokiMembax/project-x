6312703d5485d2aaf7ddc1d2e1182f12ec1dd699
PETARKING <petar.jovevski12@gmail.com>
2024-09-25 12:26:29 +0200

    fix

--- ProjectX.Database/Tables/User.sql
+++ ProjectX.Database/Tables/User.sql
@@ -3,13 +3,16 @@
 	[Id]                                 INT                     IDENTITY (1, 1) NOT NULL,
     [Uid]                                UNIQUEIDENTIFIER        NOT NULL,
     [CreatedOn]                          DATETIME                NOT NULL,
-    [DeletedOn]                          DATETIME                NULL,  
-    [Embg]                               NVARCHAR (255)          NOT NULL           UNIQUE,
+    [DeletedOn]                          DATETIME                NULL,
+    [CompanyId]                          INT                     NOT NULL,
+    [TruckId]                            INT                     NULL,
+    [TrailerId]                          INT                     NULL,
+    [Embg]                               NVARCHAR (255)          NOT NULL,
     [FirstName]                          NVARCHAR (255)          NOT NULL,
-    [LastName]                           NVARCHAR (255)          NOT NULL           UNIQUE,
+    [LastName]                           NVARCHAR (255)          NOT NULL,
     [Email]                              NVARCHAR (255)          NOT NULL,
     [PasswordHash]                       NVARCHAR (255)          NOT NULL,
-    [PhoneNumber]                        NVARCHAR (255)          NOT NULL           UNIQUE,
+    [PhoneNumber]                        NVARCHAR (255)          NOT NULL,
     [DateOfEmployment]                   DATETIME                NULL,    
     [DriversCertificateSerialNumber]     NVARCHAR (255)          NULL,
     [DriversCertificateIssueDate]        DATETIME                NULL,
@@ -23,11 +26,40 @@
     [IdentityCardSerialNumber]           NVARCHAR (255)          NULL,
     [IdentityCardIssueDate]              DATETIME                NULL,
     [IdentityCardExpiryDate]             DATETIME                NULL,
-    [CompanyId]                          INT                     NOT NULL,
-    [TruckId]                            INT                     NULL,
-    [TrailerId]                          INT                     NULL
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Company_User] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([Id]),
     CONSTRAINT [FK_Truck_User] FOREIGN KEY ([TruckId]) REFERENCES [dbo].[Truck] ([Id]),
     CONSTRAINT [FK_Trailer_User] FOREIGN KEY ([TrailerId]) REFERENCES [dbo].[Trailer] ([Id]),
 )
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniqueDriversCertificateSerialNumber
+ON [dbo].[User] (DriversCertificateSerialNumber)
+WHERE DriversCertificateSerialNumber IS NOT NULL;
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniqueDrivingLicenseSerialNumber
+ON [dbo].[User] (DrivingLicenseSerialNumber)
+WHERE DrivingLicenseSerialNumber IS NOT NULL;
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniquePassportSerialNumber
+ON [dbo].[User] (PassportSerialNumber)
+WHERE PassportSerialNumber IS NOT NULL;
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniqueIdentityCardSerialNumber
+ON [dbo].[User] (IdentityCardSerialNumber)
+WHERE IdentityCardSerialNumber IS NOT NULL;
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniqueEmbg
+ON [dbo].[User] (Embg);
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniqueEmail
+ON [dbo].[User] (Email);
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX uniquePhoneNumber
+ON [dbo].[User] (PhoneNumber);
